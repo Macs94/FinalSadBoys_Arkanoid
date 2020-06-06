bd2e71fba74699e1433b0fd65cfb9615e61b55f8
Macs94 <00309119@uca.edu.sv>
2020-06-05 22:44:28 -0600

    Minor Aesthetic Changes

--- SadArkanoid/SadArkanoid/Vista/MainMenu.Designer.cs
+++ SadArkanoid/SadArkanoid/Vista/MainMenu.Designer.cs
@@ -41,10 +41,10 @@ namespace SadArkanoid
             // TitleCard
             // 
             this.TitleCard.BackColor = System.Drawing.Color.Transparent;
-            this.TitleCard.Location = new System.Drawing.Point(170, 13);
-            this.TitleCard.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.TitleCard.Location = new System.Drawing.Point(283, 25);
+            this.TitleCard.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TitleCard.Name = "TitleCard";
-            this.TitleCard.Size = new System.Drawing.Size(332, 103);
+            this.TitleCard.Size = new System.Drawing.Size(553, 198);
             this.TitleCard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.TitleCard.TabIndex = 0;
             this.TitleCard.TabStop = false;
@@ -56,12 +56,13 @@ namespace SadArkanoid
             this.btnPlay.FlatAppearance.BorderSize = 0;
             this.btnPlay.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
             this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.btnPlay.ForeColor = System.Drawing.Color.White;
-            this.btnPlay.Location = new System.Drawing.Point(260, 199);
-            this.btnPlay.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.btnPlay.Location = new System.Drawing.Point(433, 345);
+            this.btnPlay.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnPlay.Name = "btnPlay";
-            this.btnPlay.Size = new System.Drawing.Size(134, 30);
+            this.btnPlay.Size = new System.Drawing.Size(223, 86);
             this.btnPlay.TabIndex = 1;
             this.btnPlay.Text = "1. Jugar";
             this.btnPlay.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -74,12 +75,13 @@ namespace SadArkanoid
             this.btnHighScore.FlatAppearance.BorderSize = 0;
             this.btnHighScore.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
             this.btnHighScore.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnHighScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.btnHighScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F,
+                System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.btnHighScore.ForeColor = System.Drawing.Color.White;
-            this.btnHighScore.Location = new System.Drawing.Point(260, 226);
-            this.btnHighScore.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.btnHighScore.Location = new System.Drawing.Point(433, 435);
+            this.btnHighScore.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnHighScore.Name = "btnHighScore";
-            this.btnHighScore.Size = new System.Drawing.Size(134, 40);
+            this.btnHighScore.Size = new System.Drawing.Size(223, 65);
             this.btnHighScore.TabIndex = 2;
             this.btnHighScore.Text = "2. Puntajes";
             this.btnHighScore.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -91,12 +93,13 @@ namespace SadArkanoid
             this.btnExit.FlatAppearance.BorderSize = 0;
             this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
             this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.btnExit.ForeColor = System.Drawing.Color.White;
-            this.btnExit.Location = new System.Drawing.Point(260, 259);
-            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.btnExit.Location = new System.Drawing.Point(433, 504);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(134, 34);
+            this.btnExit.Size = new System.Drawing.Size(223, 61);
             this.btnExit.TabIndex = 3;
             this.btnExit.Text = "3. Salir";
             this.btnExit.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -105,17 +108,17 @@ namespace SadArkanoid
             // 
             // MainMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(677, 335);
+            this.ClientSize = new System.Drawing.Size(1129, 644);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnHighScore);
             this.Controls.Add(this.btnPlay);
             this.Controls.Add(this.TitleCard);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "MainMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Arkanoid-Main Menu";
