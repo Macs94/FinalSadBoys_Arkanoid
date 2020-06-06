1a26b111f9039b5514ff8571739850e355f62671
kevin cea <patrizkcea503@gmail.com>
2020-06-06 11:04:43 -0600

    add: User Controller

---
+++ SadArkanoid/SadArkanoid/Controladores/UserDAO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using SadArkanoid.Modelo;
+
+namespace SadArkanoid.Controladores
+{
+    public static class UserDAO
+    {
+        public static List<User> getUsers()
+        {
+            DataTable dt = ConnectionDB.ExecuteQuery("SELECT * FROM PUBLIC.USER");
+            List<User> lista = new List<User>();
+
+            foreach (DataRow col in dt.Rows)
+            {
+                var u = new User();
+                u.username = col[0].ToString();
+                u.score = Convert.ToInt32(col[1].ToString());
+                lista.Add(u);
+            } 
+            return lista;
+        }
+        
+        public static void newUser(string uname)
+        {
+            string sql = String.Format(
+                "INSERT INTO PUBLIC.USER(username) " +
+                "VALUES('{0}');",
+                uname);
+
+            ConnectionDB.ExecuteNonQuery(sql);
+        }
+        
+        public static void updateScore(string uname, int scr)
+        {
+            string sql = String.Format(
+                "UPDATE PUBLIC.USER set score= {0} where username='{1}';",
+                scr, uname);
+            
+            ConnectionDB.ExecuteNonQuery(sql);
+        }
+
+    }
+}
---
+++ SadArkanoid/SadArkanoid/Modelo/User.cs
@@ -0,0 +1,14 @@
+﻿namespace SadArkanoid.Modelo
+{
+    public class User
+    {
+        public string username { get; set; }
+        public int score { get; set; }
+
+        public User()
+        {
+            username = "";
+            score = 0;
+        }
+    }
+}
