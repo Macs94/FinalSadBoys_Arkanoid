294ff390fa54a3d88db51e014fb2e6f413ed1333
Juan Gallardo <00091813@uca.edu.sv>
2020-06-18 23:19:31 -0600

    Modify: FormGame(Comments/Optimization)
    (cherry picked from commit ee500f39cd581eb77565213329cd54d21243ba06)

--- SadArkanoid/SadArkanoid/Modelo/GameData.cs
+++ SadArkanoid/SadArkanoid/Modelo/GameData.cs
@@ -4,11 +4,13 @@ namespace SadArkanoid.Modelo
 {
     public static class GameData
     {
+        //atributos de flujo del juego
         public static bool fullScreen { get; set; }
         public static bool gameStart { get; set; }
         public static bool gameOver { get; set; }
         public static bool victory { get; set; }
 
+        //reiniciar atributos
         public static void softReset()
         {
             gameStart = false;
